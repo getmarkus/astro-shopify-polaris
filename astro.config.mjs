146dc847ebdcbd74fc15814cede0f8973cc7e1a5
Chris Markus <chris@christophermarkus.com>
2023-12-17 14:00:45 -0500

    fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/shopify-polaris-astro',
+  base: '/astro-shopify-polaris',
   build: {
     assets: 'astro'
   },
